07c7d9ffdb3a812c1e72681b1166964ce2a7c0d4
CrysthelAparicio <crysthel.aparicio@unitec.edu>
2019-09-07 12:19:59 -0600

    Ayuda con el dropdown

--- src/components/Reservar.js
+++ src/components/Reservar.js
@@ -5,6 +5,7 @@ export class Reservar extends React.Component {
     super(props);
   }
 
+  
   render() {
     return (
       <div className="base-container" ref={this.props.containerRef}>
@@ -31,9 +32,14 @@ export class Reservar extends React.Component {
               <label htmlFor="hora">Hora</label>
               <input type="text" name="hora" placeholder="Hora" />
             </div>
-            <div className="form-group">
-              <label htmlFor="cancha">Cancha</label>
-              <input type="text" name="cancha" placeholder="Cancha" />
+            <div class="dropdown">
+              <button onclick="myFunction()" class="dropbtn">Canchas</button>
+              <div id="myDropdown" class="dropdown-content">
+                <a href="#">Don Futbol</a>
+                <a href="#">Futeca</a>
+                <a href="#">Los Amigos</a>
+                <a href="#">Soccer City</a>
+              </div>
             </div>
           </div>
         </div>
@@ -47,3 +53,22 @@ export class Reservar extends React.Component {
   }
 }
 export default Reservar;
+
+/* ESTO ES PARA QUE EL DROPDOWN DE LAS CANCHAS FUNCIONE PERO NO DA! */
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
